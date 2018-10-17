843ec6fe45510dd44d8704addc237fa96baf0d5e
PingFeng233 <35714889+PingFeng233@users.noreply.github.com>
2018-10-17 22:54:53 +0800

    平风
    #detail

--- posts/views.py
+++ posts/views.py
@@ -9,4 +9,10 @@ def index(request):
     posts = Post.objects.filter(active=True)
     categories = Category.objects.all()
     last_posts = posts.order_by('-created_time')[:5]
-    return render(request, 'index.html', locals())
+    return render(request, 'posts/index.html', locals())
+
+
+def detail(request, id):
+    post = Post.objects.get(id=id)
+    print(post)
+    return render(request, 'posts/detail.html', locals())
